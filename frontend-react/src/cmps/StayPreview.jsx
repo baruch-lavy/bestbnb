e7059f1abc89ad51c15db1c2911eb3c4549970e6
Shmuel shif <shmulikshif@gmail.com>
2025-02-12 21:37:48 +0200

    Add a demo to stay

--- frontend-react/src/cmps/StayPreview.jsx
+++ frontend-react/src/cmps/StayPreview.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export function StayPreview({ stay }) {
-    return <article className="preview">
-        <header>
-            <Link to={`/stay/${stay._id}`}>{stay.vendor}</Link>
-        </header>
+    const [currentImgIdx, setCurrentImgIdx] = useState(0)
+    const [isLiked, setIsLiked] = useState(false)
 
-        <p>Speed: <span>{stay.speed.toLocaleString()} Km/h</span></p>
-        {stay.owner && <p>Owner: <span>{stay.owner.fullname}</span></p>}
-        
-    </article>
+    return (
+        <article className="stay-preview">
+            <div className="img-container">
+                <img src={stay.imgUrls[currentImgIdx]} alt={stay.name} />
+                <div className="img-btns">
+                    <div className="guest-favorite">Guest favorite</div>
+                    <button 
+                        className={`btn-like ${isLiked ? 'active' : ''}`}
+                        onClick={() => setIsLiked(prev => !prev)}>
+                        ❤
+                    </button>
+                </div>
+            </div>
+
+            <div className="info">
+                <div className="header">
+                    <h3>{stay.loc.city}, {stay.loc.country}</h3>
+                    <div className="rating">★ {stay.reviews[0]?.rate || 'New'}</div>
+                </div>
+                <p className="distance">{stay.distance}</p>
+                <p className="dates">{stay.dates}</p>
+                <p className="price">${stay.price} <span>night</span></p>
+            </div>
+        </article>
+    )
 }
