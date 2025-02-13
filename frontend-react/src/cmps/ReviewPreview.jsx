f42c1806f91b7eb6045e38a32a16a1c3522c30e5
evyatar shlar <evyatar.szlar@gmail.com>
2025-02-13 11:00:25 +0200

    Edit Detail cmpn

--- frontend-react/src/cmps/ReviewPreview.jsx
+++ frontend-react/src/cmps/ReviewPreview.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom'
 
 export function ReviewPreview({ review }) {
-    const { byUser, aboutUser } = review
+    const { by, aboutUser } = review
 
     return <article className="preview review-preview">
-        <p>About: <Link to={`/user/${aboutUser._id}`}>{aboutUser.fullname}</Link></p>
-        <p className="review-by">By: <Link to={`/user/${byUser._id}`}>{byUser.fullname}</Link></p>
+        {/* <p>About: <Link to={`/user/${aboutUser._id}`}>{aboutUser.fullname}</Link></p> */}
+        {/* <p className="review-by">By: <Link to={`/user/${byUser._id}`}>{byUser.fullname}</Link></p> */}
+        <p className="review-by">
+             <Link to={`/user/${by._id}`}>
+             <img src={by.imgUrl} alt="" style={{ borderRadius: '50%', width: '2rem', height: '2rem', objectFit: 'cover' }}
+             /></Link>
+             {by.fullname}
+             <br />4 years on Airbnb
+             </p>
         <p className="review-txt">{review.txt}</p>
     </article>
 }
--- frontend-react/src/pages/StayDetails.jsx
+++ frontend-react/src/pages/StayDetails.jsx
@@ -6,12 +6,18 @@ import { Link } from 'react-router-dom'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { loadStay, addStayMsg } from '../store/actions/stay.actions'
 import { StayGallery } from '../cmps/StayGallery'
+import { stays } from '../data/stay.js'
+import { StayAmenities } from '../cmps/StayAmenities.jsx'
+import { ReviewList } from '../cmps/ReviewList.jsx'
+
 
 
 export function StayDetails() {
 
-  const {stayId} = useParams()
-  const stay = useSelector(storeState => storeState.stayModule.stay)
+  const { stayId } = useParams()
+  // const stay = useSelector(storeState => storeState.stayModule.stay)
+  const stay = stays[0]
+
 
   useEffect(() => {
     loadStay(stayId)
@@ -21,24 +27,42 @@ export function StayDetails() {
 
   async function onAddStayMsg(stayId) {
     try {
-        await addStayMsg(stayId, 'bla bla ' + parseInt(Math.random()*10))
-        showSuccessMsg(`Stay msg added`)
+      await addStayMsg(stayId, 'bla bla ' + parseInt(Math.random() * 10))
+      showSuccessMsg(`Stay msg added`)
     } catch (err) {
-        showErrorMsg('Cannot add stay msg')
-    }        
+      showErrorMsg('Cannot add stay msg')
+    }
 
-}
+  }
 
-if (!stay) return <p>loading</p> 
+  if (!stay) return <p>loading</p>
   return (
     <section className="stay-details">
+      <br />
+      <br />
+      <br />
       <Link to="/stay">Back to list</Link>
       <h1> {stay.name}</h1>
-       <StayGallery stay={stay}/>
+      <StayGallery stay={stay} />
       {stay && <div>
-        <h3>{stay.name}</h3>
+        <h3>{stay.type} in {stay.loc.city}, {stay.loc.country}</h3>
+        <h5>{stay.capacity} guests * 2 bedrooms * 3 beds * 1 bath</h5>
+        <h4>★ {stay.reviews[0].rate} * {stay.reviews.length} {(stay.reviews.length > 1) ? 'reviews' : 'review'}</h4>
+        
+        <div>
+        <img src={stay.host.imgUrl} alt="Host" style={{ borderRadius: '50%', width: '2rem', height: '2rem', objectFit: 'cover' }}/>
+        <h4>Hosted by {stay.host.fullname}</h4>
+        <h6>Superhost * 4 years hosting</h6>
+        </div>
+        <br />
+        <article>{stay.summary}</article>
+        <br />
+        <StayAmenities amenities = {stay.amenities}/>
+        <ReviewList  reviews={stay.reviews} />
+        {/* <Map/> */}
+        
         <h4>${stay.price}</h4>
-        <pre> {JSON.stringify(stay, null, 2)} </pre>
+        {/* <pre> {JSON.stringify(stay, null, 2)} </pre> */}
       </div>
       }
       <button onClick={() => { onAddStayMsg(stay._id) }}>Add stay msg</button>
