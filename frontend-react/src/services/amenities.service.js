ea725c925dbe4449cb56f38fd300e2116809d0c3
evyatar shlar <evyatar.szlar@gmail.com>
2025-02-15 23:40:44 +0200

    Edit amenitise name

--- frontend-react/src/services/amenities.service.js
+++ frontend-react/src/services/amenities.service.js
@@ -2,117 +2,117 @@ export const amenities = [
     {
         id: 'a1',
         label: 'Smoke alarm',
-        icon: '/img/stays/amenities/asset 1.jpg'
+        icon: '/img/stays/amenities/asset1.jpg'
     },
     {
         id: 'a2',
         label: 'Bay view',
-        icon: '/img/stays/amenities/asset 2.jpg'
+        icon: '/img/stays/amenities/asset2.jpg'
     },
     {
         id: 'a3',
         label: 'Shared beach access – Beachfront',
-        icon: '/img/stays/amenities/asset 3.jpg'
+        icon: '/img/stays/amenities/asset3.jpg'
     },
     {
         id: 'a6',
         label: 'Indoor fireplace: wood-burning',
-        icon: '/img/stays/amenities/asset 6.jpg'
+        icon: '/img/stays/amenities/asset6.jpg'
     },
     {
         id: 'a7',
         label: 'BBQ grill: gas',
-        icon: '/img/stays/amenities/asset 7.jpg'
+        icon: '/img/stays/amenities/asset7.jpg'
     },
     {
         id: 'a8',
         label: 'Exercise equipment',
-        icon: '/img/stays/amenities/asset 8.jpg'
+        icon: '/img/stays/amenities/asset8.jpg'
     },
     {
         id: 'a10',
         label: 'Crib - always at the listing',
-        icon: '/img/stays/amenities/asset 10.jpg'
+        icon: '/img/stays/amenities/asset10.jpg'
     },
     {
         id: 'a11',
         label: 'EV charger',
-        icon: '/img/stays/amenities/asset 11.jpg'
+        icon: '/img/stays/amenities/asset11.jpg'
     },
     {
         id: 'a13',
         label: 'Private pool',
-        icon: '/img/stays/amenities/asset 13.jpg'
+        icon: '/img/stays/amenities/asset13.jpg'
     },
     {
         id: 'a14',
         label: 'Private hot tub',
-        icon: '/img/stays/amenities/asset 14.jpg'
+        icon: '/img/stays/amenities/asset14.jpg'
     },
     {
         id: 'a15',
         label: 'Iron',
-        icon: '/img/stays/amenities/asset 15.jpg'
+        icon: '/img/stays/amenities/asset15.jpg'
     },
     {
         id: 'a16',
         label: 'Carbon monoxide alarm',
-        icon: '/img/stays/amenities/asset 16.jpg'
+        icon: '/img/stays/amenities/asset16.jpg'
     },
     {
         id: 'a21',
         label: 'Private entrance',
-        icon: '/img/stays/amenities/asset 21.jpg'
+        icon: '/img/stays/amenities/asset21.jpg'
     },
     {
         id: 'a22',
         label: 'Long term stays allowed',
-        icon: '/img/stays/amenities/asset 22.jpg'
+        icon: '/img/stays/amenities/asset22.jpg'
     },
     {
         id: 'a24',
         label: 'Kitchen',
-        icon: '/img/stays/amenities/asset 24.jpg'
+        icon: '/img/stays/amenities/asset24.jpg'
     },
     {
         id: 'a25',
         label: 'Wifi',
-        icon: '/img/stays/amenities/asset 25.jpg'
+        icon: '/img/stays/amenities/asset25.jpg'
     },
     {
             id: 'a43',
             label: 'Self check-in',
-            icon: '/img/stays/amenities/asset 43.jpg'
+            icon: '/img/stays/amenities/asset43.jpg'
         },
         {
             id: 'a27',
             label: 'Free parking on premises',
-            icon: '/img/stays/amenities/asset 27.jpg'
+            icon: '/img/stays/amenities/asset27.jpg'
         },
         {
             id: 'a31',
             label: 'Free washer – In building',
-            icon: '/img/stays/amenities/asset 31.jpg'
+            icon: '/img/stays/amenities/asset31.jpg'
         },
         {
             id: 'a32',
             label: 'Central air conditioning',
-            icon: '/img/stays/amenities/asset 32.jpg'
+            icon: '/img/stays/amenities/asset32.jpg'
         },
     {
         id: 'a47',
         label: 'Hair dryer',
-        icon: '/img/stays/amenities/asset 47.jpg'
+        icon: '/img/stays/amenities/asset47.jpg'
     },
     {
         id: 'a48',
         label: 'TV',
-        icon: '/img/stays/amenities/asset 48.jpg'
+        icon: '/img/stays/amenities/asset48.jpg'
     },
     {
         id: 'a50',
         label: 'Central heating',
-        icon: '/img/stays/amenities/asset 50.jpg'
+        icon: '/img/stays/amenities/asset50.jpg'
     }
 ]
 
@@ -127,187 +127,187 @@ export const amenities = [
 //     {
 //         id: 'a23',
 //         label: 's',
-//         icon: '/img/stays/amenities/asset 23.jpg'
+//         icon: '/img/stays/amenities/asset23.jpg'
 //     },
 //     {
 //         id: 'a4',
 //         label: 'age',
-//         icon: '/img/stays/amenities/asset 4.jpg'
+//         icon: '/img/stays/amenities/asset4.jpg'
 //     },
 //     {
 //         id: 'a5',
 //         label: 'house',
-//         icon: '/img/stays/amenities/asset 5.jpg'
+//         icon: '/img/stays/amenities/asset5.jpg'
 //     },
 //     {
 //         id: 'a9',
 //         label: 'ns',
-//         icon: '/img/stays/amenities/asset 9.jpg'
+//         icon: '/img/stays/amenities/asset9.jpg'
 //     },
 //     {
 //         id: 'a12',
 //         label: 'nds',
-//         icon: '/img/stays/amenities/asset 12.jpg'
+//         icon: '/img/stays/amenities/asset12.jpg'
 //     },
 //     {
 //         id: 'a17',
 //         label: 'ic',
-//         icon: '/img/stays/amenities/asset 17.jpg'
+//         icon: '/img/stays/amenities/asset17.jpg'
 //     },
 //     {
 //         id: 'a18',
 //         label: 'rt',
-//         icon: '/img/stays/amenities/asset 18.jpg'
+//         icon: '/img/stays/amenities/asset18.jpg'
 //     },
 //     {
 //         id: 'a19',
 //         label: 'ical',
-//         icon: '/img/stays/amenities/asset 19.jpg'
+//         icon: '/img/stays/amenities/asset19.jpg'
 //     },
 //     {
 //         id: 'a20',
 //         label: 'mills',
-//         icon: '/img/stays/amenities/asset 20.jpg'
+//         icon: '/img/stays/amenities/asset20.jpg'
 //     },
 //     {
 //         id: 'a26',
 //         label: 'st',
-//         icon: '/img/stays/amenities/asset 26.jpg'
+//         icon: '/img/stays/amenities/asset26.jpg'
 //     },
 //     {
 //         id: 'a28',
 //         label: 'thouse',
-//         icon: '/img/stays/amenities/asset 28.jpg'
+//         icon: '/img/stays/amenities/asset28.jpg'
 //     },
 //     {
 //         id: 'a29',
 //         label: 'ainers',
-//         icon: '/img/stays/amenities/asset 29.jpg'
+//         icon: '/img/stays/amenities/asset29.jpg'
 //     },
 //     {
 //         id: 'a30',
 //         label: 's',
-//         icon: '/img/stays/amenities/asset 30.jpg'
+//         icon: '/img/stays/amenities/asset30.jpg'
 //     },
 //     {
 //         id: 'a33',
 //         label: 'ry',
-//         icon: '/img/stays/amenities/asset 33.jpg'
+//         icon: '/img/stays/amenities/asset33.jpg'
 //     },
 //     {
 //         id: 'a34',
 //         label: 's',
-//         icon: '/img/stays/amenities/asset 34.jpg'
+//         icon: '/img/stays/amenities/asset34.jpg'
 //     },
 //     {
 //         id: 'a35',
 //         label: 'ing',
-//         icon: '/img/stays/amenities/asset 35.jpg'
+//         icon: '/img/stays/amenities/asset35.jpg'
 //     },
 //     {
 //         id: 'a36',
 //         label: '',
-//         icon: '/img/stays/amenities/asset 36.jpg'
+//         icon: '/img/stays/amenities/asset36.jpg'
 //     },
 //     {
 //         id: 'a37',
 //         label: 'ing',
-//         icon: '/img/stays/amenities/asset 37.jpg'
+//         icon: '/img/stays/amenities/asset37.jpg'
 //     },
 //     {
 //         id: 'a38',
 //         label: 'ames',
-//         icon: '/img/stays/amenities/asset 38.jpg'
+//         icon: '/img/stays/amenities/asset38.jpg'
 //     },
 //     {
 //         id: 'a39',
 //         label: 's',
-//         icon: '/img/stays/amenities/asset 39.jpg'
+//         icon: '/img/stays/amenities/asset39.jpg'
 //     },
 //     {
 //         id: 'a40',
 //         label: 'rs',
-//         icon: '/img/stays/amenities/asset 40.jpg'
+//         icon: '/img/stays/amenities/asset40.jpg'
 //     },
 //     {
 //         id: 'a41',
 //         label: 'eboats',
-//         icon: '/img/stays/amenities/asset 41.jpg'
+//         icon: '/img/stays/amenities/asset41.jpg'
 //     },
 //     {
 //         id: 'a42',
 //         label: 'ets',
-//         icon: '/img/stays/amenities/asset 42.jpg'
+//         icon: '/img/stays/amenities/asset42.jpg'
 //     },
 //     {
 //         id: 'a44',
 //         label: 's',
-//         icon: '/img/stays/amenities/asset 44.jpg'
+//         icon: '/img/stays/amenities/asset44.jpg'
 //     },
 //     {
 //         id: 'a45',
 //         label: 'li',
-//         icon: '/img/stays/amenities/asset 45.jpg'
+//         icon: '/img/stays/amenities/asset45.jpg'
 //     },
 //     {
 //         id: 'a46',
 //         label: 'adic',
-//         icon: '/img/stays/amenities/asset 46.jpg'
+//         icon: '/img/stays/amenities/asset46.jpg'
 //     },
 //     {
 //         id: 'a49',
 //         label: 'ers',
-//         icon: '/img/stays/amenities/asset 49.jpg'
+//         icon: '/img/stays/amenities/asset49.jpg'
 //     },
     
 //     {
 //         id: 'a51',
 //         label: 'ical',
-//         icon: '/img/stays/amenities/asset 51.jpg'
+//         icon: '/img/stays/amenities/asset51.jpg'
 //     },
 //     {
 //         id: 'a52',
 //         label: 'tive spaces',
-//         icon: '/img/stays/amenities/asset 52.jpg'
+//         icon: '/img/stays/amenities/asset52.jpg'
 //     },
 //     {
 //         id: 'a53',
 //         label: 'ans',
-//         icon: '/img/stays/amenities/asset 53.jpg'
+//         icon: '/img/stays/amenities/asset53.jpg'
 //     },
 //     {
 //         id: 'a54',
 //         label: 'us',
-//         icon: '/img/stays/amenities/asset 54.jpg'
+//         icon: '/img/stays/amenities/asset54.jpg'
 //     },
 //     {
 //         id: 'a55',
 //         label: 's particulares',
-//         icon: '/img/stays/amenities/asset 55.jpg'
+//         icon: '/img/stays/amenities/asset55.jpg'
 //     },
 //     {
 //         id: 'a56',
 //         label: 'ks',
-//         icon: '/img/stays/amenities/asset 56.jpg'
+//         icon: '/img/stays/amenities/asset56.jpg'
 //     },
 //     {
 //         id: 'a57',
 //         label: 'd pianos',
-//         icon: '/img/stays/amenities/asset 57.jpg'
+//         icon: '/img/stays/amenities/asset57.jpg'
 //     },
 //     {
 //         id: 'a58',
 //         label: 'grid',
-//         icon: '/img/stays/amenities/asset 58.jpg'
+//         icon: '/img/stays/amenities/asset58.jpg'
 //     },
 //     {
 //         id: 'a59',
 //         label: 'in/out',
-//         icon: '/img/stays/amenities/asset 59.jpg'
+//         icon: '/img/stays/amenities/asset59.jpg'
 //     },
 //     {
 //         id: 'a60',
 //         label: 'yards',
-//         icon: '/img/stays/amenities/asset 60.jpg'
+//         icon: '/img/stays/amenities/asset60.jpg'
 //     }
 // ] 
