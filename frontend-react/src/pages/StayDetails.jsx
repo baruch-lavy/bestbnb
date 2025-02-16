6bba81dc9858b5410aad9be33eab58b9edc83ca2
Shmuel shif <shmulikshif@gmail.com>
2025-02-16 21:40:01 +0200

    Potter's beginning in Dietles

--- frontend-react/src/pages/StayDetails.jsx
+++ frontend-react/src/pages/StayDetails.jsx
@@ -15,6 +15,7 @@ import { StayReserve } from '../cmps/StayReserve.jsx'
 
 
 
+
 export function StayDetails() {
 
   const { stayId } = useParams()
@@ -131,8 +132,46 @@ export function StayDetails() {
       {/* // } */}
       {/* <button onClick={() => { onAddStayMsg(stay._id) }}>Add stay msg</button> */}
 
-      <footer>
-        this is footer
+      <footer className="details-footer">
+        <div className="support-content">
+          <div className="support-links">
+            <h3>Support</h3>
+            <ul>
+              <li><a href="#">Help Center</a></li>
+              <li><a href="#">AirCover</a></li>
+              <li><a href="#">Anti-discrimination</a></li>
+              <li><a href="#">Disability support</a></li>
+              <li><a href="#">Cancellation options</a></li>
+              <li><a href="#">Report neighborhood concern</a></li>
+            </ul>
+          </div>
+
+          <div className="support-links">
+            <h3>Hosting</h3>
+            <ul>
+              <li><a href="#">Bestbnb your home</a></li>
+              <li><a href="#">AirCover for Hosts</a></li>
+              <li><a href="#">Hosting resources</a></li>
+              <li><a href="#">Community forum</a></li>
+              <li><a href="#">Hosting responsibly</a></li>
+              <li><a href="#">Bestbnb-friendly apartments</a></li>
+              <li><a href="#">Join a free Hosting class</a></li>
+              <li><a href="#">Find a co-host</a></li>
+            </ul>
+          </div>
+
+          <div className="support-links">
+            <h3>Bestbnb</h3>
+            <ul>
+              <li><a href="#">Newsroom</a></li>
+              <li><a href="#">New features</a></li>
+              <li><a href="#">Careers</a></li>
+              <li><a href="#">Investors</a></li>
+              <li><a href="#">Gift cards</a></li>
+              <li><a href="#">Bestbnb.org emergency stays</a></li>
+            </ul>
+          </div>
+        </div>
       </footer>
     </section>
   )
