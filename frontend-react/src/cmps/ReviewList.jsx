7aa78f3b125a95cf912f21472019f0780166700e
evyatar shlar <evyatar.szlar@gmail.com>
2025-03-09 12:48:19 +0200

    Refactor ReviewList component: update action button visibility logic and fix review key prop

--- frontend-react/src/cmps/ReviewList.jsx
+++ frontend-react/src/cmps/ReviewList.jsx
@@ -4,22 +4,22 @@ import { ReviewPreview } from './ReviewPreview.jsx'
 
 export function ReviewList({ reviews, onRemoveReview }) {
     
-    // function shouldShowActionBtns(review) {
-    //     const user = userService.getLoggedinUser()
+    function shouldShowActionBtns(review) {
+        const user = userService.getLoggedinUser()
         
-    //     if (!user) return false
-    //     if (user.isAdmin) return true
-    //     return review.byUser?._id === user._id
-    // }
+        if (!user) return false
+        if (user.isAdmin) return true
+        return review.by?._id === user._id
+    }
 
     return <section className='reviw-list-main'>
         <ul className="review-list">
             {reviews.slice(0 , 6).map(review =>
-                <li key={review.at}>
+                <li key={review.id}>
                     <ReviewPreview review={review}/>
-                    {/* {shouldShowActionBtns(review) && <div className="actions">
+                    {shouldShowActionBtns(review) && <div className="actions">
                         <button onClick={() => onRemoveReview(review._id)}>x</button>
-                    </div>} */}
+                    </div>}
                 </li>)
             }
         </ul>
