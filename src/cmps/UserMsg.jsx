8010f7c476a6f9e5ab8b6e0dcc9f5f7147681fed
evyatar shlar <evyatar.szlar@gmail.com>
2025-03-23 14:35:03 +0200

    Refactor UserMsg component to update socket

--- src/cmps/UserMsg.jsx
+++ src/cmps/UserMsg.jsx
@@ -1,6 +1,6 @@
 import { eventBus, showSuccessMsg } from '../services/event-bus.service'
 import { useState, useEffect, useRef } from 'react'
-import { socketService, SOCKET_EVENT_REVIEW_ABOUT_YOU } from '../services/socket.service'
+import { socketService } from '../services/socket.service'
 
 export function UserMsg() {
 	const [msg, setMsg] = useState(null)
@@ -16,13 +16,14 @@ export function UserMsg() {
 			timeoutIdRef.current = setTimeout(closeMsg, 3000)
 		})
 
-		socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
-			showSuccessMsg(`New review about me ${review.txt}`)
+		socketService.on('neworeder', order => {
+			console.log('new order', order)
+			showSuccessMsg(`Your host is ${order.status} your order`)
 		})
 
 		return () => {
 			unsubscribe()
-			socketService.off(SOCKET_EVENT_REVIEW_ABOUT_YOU)
+			socketService.off('neworeder')
 		}
 	}, [])
 
