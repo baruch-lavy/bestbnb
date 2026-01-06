198caadbd8e8725285d5993ae801dc925018532a
evyatar shlar <evyatar.szlar@gmail.com>
2026-01-06 13:43:33 +0200

    Fix capitalization of alt text for share and save buttons in StayGallery

--- src/pages/StayGallery.jsx
+++ src/pages/StayGallery.jsx
@@ -46,11 +46,11 @@ export function StayGallery() {
 
           <div className="action-btns">
             <button className="action-btn" onClick={handleShare}>
-              <img src="/img/stays/share.svg" alt="Share" />
+              <img src="/img/stays/share.svg" alt="share" />
               <span>Share</span>
             </button>
             <button className="action-btn">
-              <img src="/img/stays/heart.svg" alt="Save" />
+              <img src="/img/stays/heart.svg" alt="save" />
               <span>Save</span>
             </button>
           </div>
